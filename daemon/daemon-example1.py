668072f51d0ba4331e8a40ec6ebdadbddd47d531
xiaopeng <passionlv@163.com>
2018-08-12 22:48:21 +0800

    modify

--- daemon/daemon-example1.py
+++ daemon/daemon-example1.py
@@ -3,6 +3,9 @@ import logging
 import time
 import signal
 import threading
+import os
+import sys
+import lockfile
 
 logfilename = '/home/xiaopeng/tmp/testdaemon.log'
 logger = logging.getLogger("DaemonLog")
@@ -18,9 +21,20 @@ def initial_program_setup():
     logger.info('daemon started')
 
 def do_main_program():
+    logger.info("In the main programe: %s, which ppid: %s" % (os.getpid(), os.getppid()))
+    n = 0
     while True:
         time.sleep(1)
-        logger.info('another second passed')
+        print("In the main programe: %s, which ppid: %s, n=%r" % (os.getpid(), os.getppid(), n ))
+        logger.info("In the main programe: %s, which ppid: %s, n=%r" % (os.getpid(), os.getppid(), n ))
+        n = n + 1
+        if n > 10:
+            #pass
+            #raise NotImplementedError
+            #sys.exit(0)
+            break;
+        #logger.info("In the main program")
+        #logger.info('another second passed')
 
 def program_cleanup(signum, frame):
     logger.info('daemon stops')
@@ -29,7 +43,12 @@ def program_cleanup(signum, frame):
 def reload_program_config(signum, frame):
     logger.info('reloading config')
 
-context = daemon.DaemonContext()
+pidfile= lockfile.FileLock('/home/xiaopeng/tmp/example1.pid')
+# 如果设定了stdout，stderr,
+# context = daemon.DaemonContext(stdout=sys.stdout,
+#                                 stderr=sys.stderr)
+
+context = daemon.DaemonContext(pidfile=pidfile)
 
 context.signal_map = {
     signal.SIGTERM: program_cleanup,
@@ -37,9 +56,16 @@ context.signal_map = {
     signal.SIGUSR1: reload_program_config,
     }
 
+
 context.files_preserve = [handler.stream]
 
 initial_program_setup()
 
+logger.info("In the first process: %s, which ppid: %s" %(os.getpid(), os.getppid()))
+
 with context:
+    logger.info("In context: %s, which ppid: %s" % (os.getpid(), os.getppid()))
+    logger.info("enter the main program")
+
     do_main_program()
+    logger.info("exit the main program")
